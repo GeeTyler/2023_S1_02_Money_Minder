fdf665b9df77f6f8ff8a73968dd86a01f00201c6
Tyler Gee <tyeschool@hotmail.com>
2023-04-29 12:18:09 +1200

    added identity

---
+++ MoneyMinder/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using static MoneyMinder.Areas.Identity.Pages.Account.RegisterModel;
+
+namespace MoneyMinder.Areas.Identity.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public void OnGet()
+        {
+            ReturnUrl = Url.Content("~/");
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            ReturnUrl = Url.Content("/Home");
+            
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password,
+                    false, lockoutOnFailure: false);
+                if(result.Succeeded) return Redirect(ReturnUrl);
+            }
+
+            return Page();
+        }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
--- MoneyMinder/Startup.cs
+++ MoneyMinder/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MoneyMinder.Data;
 using MoneyMinder.Model;
 using System;
 using System.Collections.Generic;
