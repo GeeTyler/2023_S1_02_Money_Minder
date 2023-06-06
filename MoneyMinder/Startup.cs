9540d9b6774bcf9ddbef2cb7b13f6da26b78448d
arzmo <84154500+arzmo@users.noreply.github.com>
2023-06-06 18:11:46 +1200

    Update Startup.cs

--- MoneyMinder/Startup.cs
+++ MoneyMinder/Startup.cs
@@ -48,6 +48,8 @@ namespace MoneyMinder
             })
             .AddDefaultTokenProviders()
             .AddEntityFrameworkStores<DatabaseContext>();
+            
+            services.AddSingleton<IJSRuntime, JSRuntime>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
